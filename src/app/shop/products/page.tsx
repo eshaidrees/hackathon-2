7d3a23c3826bc8b450b180a2b630d615af593542
eshaidrees <eshaidrees190@gmail.com>
2024-12-09 01:22:19 +0500

    Update

--- src/app/shop/products/page.tsx
+++ src/app/shop/products/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export const Products = () => {
+export default function Products () {
 
 const ProductData = [
     {
@@ -115,4 +115,3 @@ const ProductData = [
   )
 }
 
-export default Products
